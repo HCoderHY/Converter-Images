1fae4c2be519299d75106ee5b6cd0a94dfda1e33
Bogdan Tanov <86558426+BogdanTanov@users.noreply.github.com>
2021-12-12 23:58:18 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PIL import Image
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1366, 392)
+        font = QtGui.QFont()
+        font.setFamily("Particle")
+        font.setPointSize(50)
+        MainWindow.setFont(font)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(20, 60, 1321, 81))
+        font = QtGui.QFont()
+        font.setFamily("Particle")
+        font.setPointSize(40)
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet("background: #221F1F; color: white; border: 0px solid white;")
+        self.pushButton.setObjectName("pushButton")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(0, 0, 1366, 50))
+        font = QtGui.QFont()
+        font.setFamily("Particle")
+        font.setPointSize(30)
+        self.label.setFont(font)
+        self.label.setStyleSheet("background: #221F1F; color: white;")
+        self.label.setObjectName("label")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(230, 290, 911, 81))
+        font = QtGui.QFont()
+        font.setFamily("Particle")
+        font.setPointSize(40)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setStyleSheet("background: #221F1F; color: white; border: 0px solid white;")
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(100, 160, 1151, 41))
+        font = QtGui.QFont()
+        font.setFamily("Century Gothic")
+        font.setPointSize(23)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setStyleSheet("color: #221F1F; border: 2px solid #221F1F; background: white;")
+        self.lineEdit.setText("")
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(100, 230, 1151, 41))
+        font = QtGui.QFont()
+        font.setFamily("Century Gothic")
+        font.setPointSize(23)
+        self.lineEdit_2.setFont(font)
+        self.lineEdit_2.setStyleSheet("color: #221F1F; border: 2px solid #221F1F; background: white;")
+        self.lineEdit_2.setText("")
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.func()
+
+    def func(self):
+        self.pushButton.clicked.connect(self.exit_in_program)
+        self.pushButton_2.clicked.connect(self.converted)
+
+    def converted(self):
+        img = Image.open(self.lineEdit.text())
+        img.save(self.lineEdit_2.text())
+
+    def exit_in_program(self):
+        exit()
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Converter Images 0.0.1"))
+        self.pushButton.setText(_translate("MainWindow", "CLOSE"))
+        self.label.setText(_translate("MainWindow", "                                                   Converter Images"))
+        self.pushButton_2.setText(_translate("MainWindow", "Converted"))
+        self.lineEdit.setPlaceholderText(_translate("MainWindow", "Write old link and old image..."))
+        self.lineEdit_2.setPlaceholderText(_translate("MainWindow", "Write new link and new image and new name for file..."))
+import sys
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+sys.exit(app.exec_())
